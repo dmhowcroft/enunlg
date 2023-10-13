d71a4e76d997822742391714ce01b9cf95b8c961
Dave Howcroft <dave.howcroft@gmail.com>
2023-10-13 12:11:12 +0100

    continue creating general seq2seq+attn support

--- scripts/seq2seq+attn.py
+++ scripts/seq2seq+attn.py
@@ -10,6 +10,24 @@ import enunlg.trainer
 import enunlg.vocabulary
 
 
+class NNGenerator(object):
+    def __init__(self, input_vocab, output_vocab, model_class, model_config):
+        self.input_vocab = input_vocab
+        self.output_vocab = output_vocab
+        self.model = model_class(model_config)
+
+    def generate_from_input_seq(self, input_seq):
+        return self._output_bridge(self.model.generate(self._input_bridge(input_seq)))
+        pass
+
+    def _input_bridge(self, input_seq):
+        """convert input into input appropriate for self.model"""
+
+    def _output_bridge(self, output_seq):
+        """Convert raw output of self.model into output"""
+        pass
+
+
 @hydra.main(version_base=None, config_path='../config', config_name='seq2seq+attn')
 def seq2seq_attn_main(config: omegaconf.DictConfig):
     hydra_managed_output_dir = hydra.core.hydra_config.HydraConfig.get().runtime.output_dir
@@ -40,10 +58,10 @@ def seq2seq_attn_main(config: omegaconf.DictConfig):
     train_embeddings = prep_embeddings(input_vocab, output_vocab, train)
     dev_embeddings = prep_embeddings(input_vocab, output_vocab, dev)
 
-    model = s2s.TGenEncDec(input_vocab, output_vocab, model_config=config.model)
+    model = s2s.Seq2SeqAttn(input_vocab.size, output_vocab.size, model_config=config.model)
     print(model)
 
-    trainer = enunlg.trainer.TGenTrainer(model, training_config=config.mode.train)
+    trainer = enunlg.trainer.Seq2SeqAttnTrainer(model, training_config=config.mode.train, input_vocab=input_vocab, output_vocab=output_vocab)
 
     trainer.train_iterations(train_embeddings, dev_embeddings)
 
