dc230c1d617aab5a0bc70f63ef249ab9325cf2ec
Dave Howcroft <dave.howcroft@gmail.com>
2023-11-01 19:57:23 +0000

    enhance: add print_summary_stats()

--- enunlg/data_management/pipelinecorpus.py
+++ enunlg/data_management/pipelinecorpus.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from typing import Any, Callable, Dict, Iterable, List, Optional, TypeVar, Union
 
 import enunlg.data_management.iocorpus
@@ -55,3 +56,16 @@ class PipelineCorpus(enunlg.data_management.iocorpus.IOCorpus):
     def items_by_layer(self, layer_name):
         for item in self:
             yield item[layer_name]
+
+    def print_summary_stats(self):
+        print(f"{self.metadata=}")
+        print(", ".join(self.layers))
+        print(f"num entries: {len(self)}")
+        num_entries_per_layer = defaultdict(int)
+        layer_stats = defaultdict(int)
+        for layer in self.layers:
+            for entry in self:
+                layer_stats[layer] += len(entry[layer])
+                num_entries_per_layer[layer] += 1
+        for layer in layer_stats:
+            print(f"{layer}:\t\t{layer_stats[layer] / num_entries_per_layer[layer]} ({num_entries_per_layer[layer]})")
