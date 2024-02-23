2599083e4155220710f000258d930affc85e96c5
Your Name <dave.howcroft@gmail.com>
2024-02-23 15:11:32 +0000

    enhance: add support for loading sfx-restaurant data

--- enunlg/data_management/loader.py
+++ enunlg/data_management/loader.py
@@ -2,6 +2,7 @@ from typing import TYPE_CHECKING
 
 import logging
 
+import enunlg.data_management.cued
 import enunlg.data_management.e2e_challenge
 import enunlg.data_management.enriched_e2e
 import enunlg.data_management.enriched_webnlg
@@ -11,21 +12,23 @@ if TYPE_CHECKING:
 
 logger = logging.getLogger(__name__)
 
-SUPPORTED_DATASETS = {"e2e", "e2e-cleaned", "enriched-e2e", "enriched-webnlg"}
+SUPPORTED_DATASETS = {"e2e", "e2e-cleaned", "enriched-e2e", "enriched-webnlg", "sfx-restaurant"}
 
 
 def load_data_from_config(data_config: "omegaconf.DictConfig"):
     """Selects the right function to use to load the desired data and return a corpus.
 
     In an experiment's YAML file, the data config is specified under the key `data`.
-    Expected properties are a name (string), a list of (named) splits (as strings), and an `input mode`,
-    specifying whether to use the `e2e` input mode (i.e. SlotValueMRs) or `rdf` input mode.
+    Expected properties are `corpus`, which has a name (string) and a list of (named) splits (as strings).
+    The `data` node also has an `input mode`, specifying whether to use the `e2e` input mode (i.e. SlotValueMRs)
+    or `rdf` input mode.
 
     Example
     -------
     data:
-      name: enriched-e2e
-      splits: [train]
+      corpus:
+        name: enriched-e2e
+        splits: [train]
       input_mode: e2e
     """
     if data_config.corpus.name not in SUPPORTED_DATASETS:
@@ -42,5 +45,8 @@ def load_data_from_config(data_config: "omegaconf.DictConfig"):
     elif data_config.corpus.name == 'enriched-webnlg':
         logger.info("Loading Enriched WebNLG (v1.6) Data...")
         return enunlg.data_management.enriched_webnlg.load_enriched_webnlg(data_config.corpus.splits)
+    elif data_config.corpus.name == 'sfx-restaurant':
+        logger.info("Loading SFX Restaurant data...")
+        return enunlg.data_management.cued.load_sfx_restaurant(data_config.corpus.splits)
     else:
         raise ValueError("We can only load the Enriched E2E and Enriched WebNLG datasets right now.")
