7098adf592c97a246e480dad94d6cf6d04b53616
Your Name <dave.howcroft@gmail.com>
2024-03-12 14:26:18 +0000

    refactor: extract preprocessing to a function

--- scripts/tgen_classifier.py
+++ scripts/tgen_classifier.py
@@ -12,11 +12,11 @@ from enunlg.normalisation.tokenisation import TGenTokeniser
 
 import enunlg
 import enunlg.data_management.e2e_challenge as e2e
-import enunlg.embeddings.binary as onehot
+import enunlg.embeddings.binary
 import enunlg.meaning_representation.dialogue_acts as das
 import enunlg.util
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("enunlg-scripts.tgen_classifier")
 
 MAX_INPUT_LENGTH_IN_KV_PAIRS = 10
 MAX_INPUT_LENGTH_IN_INDICES = 3 * MAX_INPUT_LENGTH_IN_KV_PAIRS
@@ -35,6 +35,26 @@ def prep_tgen_text_integer_reps(input_corpus):
 SUPPORTED_DATASETS = {"e2e", "e2e-cleaned"}
 
 
+def preprocess(corpus, preprocessing_config):
+    if preprocessing_config.text.normalise == 'tgen':
+        corpus = e2e.E2ECorpus([e2e.E2EPair(pair.mr, TGenTokeniser.tokenize(pair.text)) for pair in corpus])
+    if preprocessing_config.text.delexicalise:
+        logger.info('Applying delexicalisation...')
+        if preprocessing_config.text.delexicalise.mode == 'split_on_caps':
+            logger.info(f'...splitting on capitals in values')
+            logger.info(f"...delexicalising: {preprocessing_config.text.delexicalise.slots}")
+            corpus = e2e.E2ECorpus([e2e.delexicalise_exact_matches(pair,
+                                                                   fields_to_delex=preprocessing_config.text.delexicalise.slots)
+                                    for pair in corpus])
+        else:
+            raise ValueError("We can only handle the mode where we also check splitting on caps for values right now.")
+    if preprocessing_config.mr.ignore_order:
+        logger.info("Sorting slot-value pairs in the MR to ignore order...")
+        corpus.sort_mr_elements()
+    return corpus
+    
+
+
 @hydra.main(version_base=None, config_path='../config', config_name='tgen_classifier')
 def tgen_classifier_main(config: omegaconf.DictConfig) -> None:
     # Add Hydra-managed output dir to the config dictionary
@@ -50,9 +70,11 @@ def tgen_classifier_main(config: omegaconf.DictConfig) -> None:
     elif config.mode == "parameters":
         train_tgen_classifier(config, shortcircuit="parameters")
     elif config.mode == "test":
-        test_tgen_classifier(config)
+        # test_tgen_classifier(config)
+        pass
     else:
         raise ValueError(f"Expected config.mode to specify `train` or `parameters` modes.")
+
 
 def train_tgen_classifier(config: omegaconf.DictConfig, shortcircuit=None):
     enunlg.util.set_random_seeds(config.random_seed)
@@ -61,28 +83,14 @@ def train_tgen_classifier(config: omegaconf.DictConfig, shortcircuit=None):
     corpus.print_summary_stats()
     print("____________")
 
-    if config.preprocessing.text.normalise == 'tgen':
-        corpus = e2e.E2ECorpus([e2e.E2EPair(pair.mr, TGenTokeniser.tokenize(pair.text)) for pair in corpus])
-    if config.preprocessing.text.delexicalise:
-        logger.info('Applying delexicalisation...')
-        if config.preprocessing.text.delexicalise.mode == 'split_on_caps':
-            logger.info(f'...splitting on capitals in values')
-            logger.info(f"...delexicalising: {config.preprocessing.text.delexicalise.slots}")
-            corpus = e2e.E2ECorpus([e2e.delexicalise_exact_matches(pair,
-                                                                   fields_to_delex=config.preprocessing.text.delexicalise.slots)
-                                    for pair in corpus])
-        else:
-            raise ValueError("We can only handle the mode where we also check splitting on caps for values right now.")
-    if config.preprocessing.mr.ignore_order:
-        logger.info("Sorting slot-value pairs in the MR to ignore order...")
-        corpus.sort_mr_elements()
+    corpus = preprocess(corpus, config.preprocessing)
 
     logger.info("Preparing training data for PyTorch...")
     # Prepare mr/input integer representation
     token_int_mapper = prep_tgen_text_integer_reps(corpus)
     # Prepare onehot encoding
     multi_da_mrs = [das.MultivaluedDA.from_slot_value_list('inform', mr.items()) for mr, _ in corpus]
-    onehot_encoder = onehot.DialogueActEmbeddings(multi_da_mrs, collapse_values=False)
+    onehot_encoder = enunlg.embeddings.binary.DialogueActEmbeddings(multi_da_mrs, collapse_values=False)
     train_mr_onehots = [onehot_encoder.embed_da(mr) for mr in multi_da_mrs]
     # Prepare text/output integer representation
     train_tokens = [text.strip().split() for _, text in corpus]
@@ -100,12 +108,11 @@ def train_tgen_classifier(config: omegaconf.DictConfig, shortcircuit=None):
     enunlg.util.log_sequence(train_text_ints[:10], indent="... ")
 
 
-    logger.info(f"Preparing neural network using {config.pytorch.device=}")
-    DEVICE = config.pytorch.device
-    tgen_classifier = binary_mr_classifier.TGenSemClassifier(token_int_mapper, onehot_encoder).to(DEVICE)
+    logger.info(f"Preparing neural network...")
+    tgen_classifier = binary_mr_classifier.TGenSemClassifier(token_int_mapper, onehot_encoder)
 
-    training_pairs = [(torch.tensor(enc_emb, dtype=torch.long, device=DEVICE),
-                       torch.tensor(dec_emb, dtype=torch.float, device=DEVICE))
+    training_pairs = [(torch.tensor(enc_emb, dtype=torch.long),
+                       torch.tensor(dec_emb, dtype=torch.float))
                       for enc_emb, dec_emb in zip(train_text_ints, train_mr_onehots)]
 
     logger.info(f"Running {config.train.num_epochs} epochs of {len(training_pairs)} iterations (looking at each training pair once per epoch)")
@@ -115,4 +122,4 @@ def train_tgen_classifier(config: omegaconf.DictConfig, shortcircuit=None):
 
 
 if __name__ == "__main__":
-    corpus = tgen_classifier_main()
+    tgen_classifier_main()
