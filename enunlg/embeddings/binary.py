09660709ce369ea8b7332d52c06ff8c534f13738
Dave Howcroft <dave.howcroft@gmail.com>
2024-03-20 15:51:13 +0000

    add support for saving bitvector encodings (e.g. for sclstm)

--- enunlg/embeddings/binary.py
+++ enunlg/embeddings/binary.py
@@ -1,8 +1,11 @@
 from typing import Dict, Iterable, List, MutableMapping, Optional, Sequence, Set, Text, Tuple, Union, TYPE_CHECKING
 
 import logging
+import os
+import tarfile
 
 import bidict
+import omegaconf
 
 if TYPE_CHECKING:
     import enunlg.meaning_representation.dialogue_acts as dialogue_acts
@@ -11,6 +14,8 @@ logger = logging.getLogger(__name__)
 
 
 class DialogueActEmbeddings(object):
+    STATE_ATTRIBUTES = ("_collapse_values", "_acts", "_slot_value_pairs", "_unk_sv_pair", "acts", "slot_value_pairs")
+
     def __init__(self, multi_da_seq: Iterable["dialogue_acts.MultivaluedDA"], collapse_values=True) -> None:
         """Designed to create embeddings compatible with SC-LSTM."""
         self._collapse_values = collapse_values
@@ -91,3 +96,31 @@ class DialogueActEmbeddings(object):
         for slot_value_index in slot_value_indices:
             retval.append(str(self.slot_value_pairs.inv[slot_value_index]))
         return " ".join(retval)
+
+    def _save_classname_to_dir(self, directory_path):
+        with open(os.path.join(directory_path, "__class__.__name__"), 'w') as class_file:
+            class_file.write(self.__class__.__name__)
+
+    def save(self, filepath, tgz=False):
+        os.mkdir(filepath)
+        self._save_classname_to_dir(filepath)
+        state = {}
+        for attribute in self.STATE_ATTRIBUTES:
+            curr_obj = getattr(self, attribute)
+            save_method = getattr(curr_obj, 'save', None)
+            if attribute in ("acts", "slot_value_pairs"):
+                # These are bidicts, so we'll save them as dicts
+                state[attribute] = {str(k): curr_obj[k] for k in curr_obj}
+            elif save_method is None:
+                if isinstance(curr_obj, set):
+                    state[attribute] = tuple(curr_obj)
+                else:
+                    state[attribute] = curr_obj
+            else:
+                state[attribute] = f"./{attribute}"
+                curr_obj.save(f"{filepath}/{attribute}")
+        with open(os.path.join(filepath, "_save_state.yaml"), 'w') as state_file:
+            omegaconf.OmegaConf.save(state, state_file)
+        if tgz:
+            with tarfile.open(f"{filepath}.tgz", mode="x:gz") as out_file:
+                out_file.add(filepath, arcname=os.path.basename(filepath))
