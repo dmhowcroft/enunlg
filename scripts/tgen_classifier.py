fcf8d792927c23da719c827822204f1035cda0b8
Dave Howcroft <dave.howcroft@gmail.com>
2024-04-03 02:23:23 +0100

    refactor: use a convenience class to group models with vocabs for this classifier

--- scripts/tgen_classifier.py
+++ scripts/tgen_classifier.py
@@ -3,6 +3,8 @@
 from pathlib import Path
 
 import logging
+import tarfile
+import tempfile
 
 import hydra
 import numpy as np
@@ -28,6 +30,70 @@ MAX_INPUT_LENGTH_IN_INDICES = 3 * MAX_INPUT_LENGTH_IN_KV_PAIRS
 HIDDEN_LAYER_SIZE = 50
 
 EPOCHS = 20
+
+
+class FullBinaryMRClassifier(object):
+    STATE_ATTRIBUTES = ('text_vocab', 'binary_mr_vocab', 'model')
+
+    def __init__(self, text_vocab: enunlg.vocabulary.TokenVocabulary,
+                 binary_mr_vocab: enunlg.embeddings.binary.DialogueActEmbeddings,
+                 model_config: omegaconf.DictConfig):
+        """
+        Create a classifier and it's associated files
+        """
+        self.text_vocab = text_vocab
+        self.binary_mr_vocab = binary_mr_vocab
+        # Store some basic information about the corpus
+        self.model = binary_mr_classifier.TGenSemClassifier(self.text_vocab.size,
+                                                            self.binary_mr_vocab.dimensionality,
+                                                            model_config)
+
+    @property
+    def model_config(self):
+        return self.model.config
+
+    def predict(self, text_ints):
+        return self.model.predict(text_ints).squeeze(0).squeeze(0).tolist()
+
+    def _save_classname_to_dir(self, directory_path):
+        with (Path(directory_path) / "__class__.__name__").open('w') as class_file:
+            class_file.write(self.__class__.__name__)
+
+    def save(self, filepath, tgz=True):
+        Path(filepath).mkdir()
+        self._save_classname_to_dir(filepath)
+        state = {}
+        for attribute in self.STATE_ATTRIBUTES:
+            curr_obj = getattr(self, attribute)
+            save_method = getattr(curr_obj, 'save', None)
+            if save_method is None:
+                state[attribute] = curr_obj
+            else:
+                state[attribute] = f"./{attribute}"
+                curr_obj.save(f"{filepath}/{attribute}", tgz=False)
+        if tgz:
+            with tarfile.open(f"{filepath}.tgz", mode="x:gz") as out_file:
+                out_file.add(filepath, arcname=Path(filepath).parent)
+
+    @classmethod
+    def load(cls, filepath):
+        if tarfile.is_tarfile(filepath):
+            with tarfile.open(filepath, 'r') as generator_file:
+                tmp_dir = tempfile.mkdtemp()
+                tarfile_member_names = generator_file.getmembers()
+                generator_file.extractall(tmp_dir)
+                root_name = Path(tarfile_member_names[0].name).parts[0]
+                p = (Path(tmp_dir) / root_name / "__class__.__name__")
+                print(p)
+                with p.open('r') as class_name_file:
+                    class_name = class_name_file.read().strip()
+                    assert class_name == cls.__name__, f"{class_name} != {cls.__name__}"
+                model = enunlg.nlu.binary_mr_classifier.TGenSemClassifier.load_from_dir(Path(tmp_dir) / root_name / 'model')
+                text_vocab = enunlg.vocabulary.TokenVocabulary.load_from_dir(Path(tmp_dir) / root_name / 'text_vocab')
+                binary_mr_vocab = enunlg.embeddings.binary.DialogueActEmbeddings.load_from_dir(Path(tmp_dir) / root_name / 'binary_mr_vocab')
+                classifier = cls(text_vocab, binary_mr_vocab, model.config)
+                classifier.model = model
+                return classifier
 
 
 def prep_tgen_text_integer_reps(input_corpus):
@@ -95,13 +161,14 @@ def train_tgen_classifier(config: omegaconf.DictConfig, shortcircuit=None):
     validation_corpus = preprocess(validation_corpus, config.preprocessing)
 
     logger.info("Preparing training data for PyTorch...")
-    # Prepare mr/input integer representation
+    # Prepare input integer representation
     token_int_mapper = prep_tgen_text_integer_reps(corpus)
     # Prepare bitvector encoding
     multi_da_mrs = [das.MultivaluedDA.from_slot_value_list('inform', mr.items()) for mr, _ in corpus]
     bitvector_encoder = enunlg.embeddings.binary.DialogueActEmbeddings(multi_da_mrs, collapse_values=False)
-    train_mr_bitvectors = [bitvector_encoder.embed_da(mr) for mr in multi_da_mrs]
+
     # Prepare text/output integer representation
+    train_mr_bitvectors = [bitvector_encoder.embed_da(mr) for mr in multi_da_mrs]
     train_tokens = [text.strip().split() for _, text in corpus]
     text_lengths = [len(text) for text in train_tokens]
     train_text_ints = [token_int_mapper.get_ints_with_left_padding(text.split()) for _, text in corpus]
@@ -116,13 +183,12 @@ def train_tgen_classifier(config: omegaconf.DictConfig, shortcircuit=None):
     logger.info("The same texts as lists of vocab indices")
     enunlg.util.log_sequence(train_text_ints[:10], indent="... ")
 
-    logger.info("Preparing neural network...")
-    tgen_classifier = binary_mr_classifier.TGenSemClassifier(token_int_mapper.size, bitvector_encoder.dimensionality, config.model)
-    total_parameters = enunlg.util.count_parameters(tgen_classifier)
+    classifier = FullBinaryMRClassifier(token_int_mapper, bitvector_encoder, config.model)
+    total_parameters = enunlg.util.count_parameters(classifier.model)
     if shortcircuit == 'parameters':
         exit()
 
-    trainer = enunlg.trainer.binary_mr_classifier.BinaryMRClassifierTrainer(tgen_classifier, config.train, token_int_mapper, bitvector_encoder)
+    trainer = enunlg.trainer.binary_mr_classifier.BinaryMRClassifierTrainer(classifier.model, config.train, token_int_mapper, bitvector_encoder)
 
     training_pairs = [(torch.tensor(enc_emb, dtype=torch.long),
                        torch.tensor(dec_emb, dtype=torch.float))
@@ -137,9 +203,7 @@ def train_tgen_classifier(config: omegaconf.DictConfig, shortcircuit=None):
     logger.info(f"Running {config.train.num_epochs} epochs of {len(training_pairs)} iterations (with {len(validation_pairs)} validation pairs")
     losses_for_plotting = trainer.train_iterations(training_pairs, validation_pairs)
 
-    tgen_classifier.save(Path(config.output_dir) / f'trained_{tgen_classifier.__class__.__name__}.nlg')
-    token_int_mapper.save(Path(config.output_dir) / f'{token_int_mapper.__class__.__name__}.nlg')
-    bitvector_encoder.save(Path(config.output_dir) / f'{bitvector_encoder.__class__.__name__}.nlg')
+    classifier.save(Path(config.output_dir) / f'trained_{classifier.__class__.__name__}.nlg')
 
 
 def test_tgen_classifier(config: omegaconf.DictConfig, shortcircuit=None):
@@ -151,20 +215,16 @@ def test_tgen_classifier(config: omegaconf.DictConfig, shortcircuit=None):
 
     corpus = preprocess(corpus, config.preprocessing)
 
-    # Load token_int_mapper
-    token_int_mapper = enunlg.vocabulary.TokenVocabulary.load_from_dir(Path(config.test.classifier_file).parent / "TokenVocabulary.nlg")
-    # Load bitvector_encoder
-    bitvector_encoder = enunlg.embeddings.binary.DialogueActEmbeddings.load_from_dir(Path(config.test.classifier_file).parent / "DialogueActEmbeddings.nlg")
+
+    classifier = FullBinaryMRClassifier.load(config.test.classifier_file)
 
     # Prepare text/output integer representation
     test_tokens = [text.strip().split() for _, text in corpus]
-    test_text_ints = [token_int_mapper.get_ints_with_left_padding(text) for text in test_tokens]
+    test_text_ints = [classifier.text_vocab.get_ints_with_left_padding(text) for text in test_tokens]
     multi_da_mrs = [das.MultivaluedDA.from_slot_value_list('inform', mr.items()) for mr, _ in corpus]
-    test_mr_bitvectors = [bitvector_encoder.embed_da(mr) for mr in multi_da_mrs]
+    test_mr_bitvectors = [classifier.binary_mr_vocab.embed_da(mr) for mr in multi_da_mrs]
 
-    logger.info("Loading neural network...")
-    tgen_classifier = binary_mr_classifier.TGenSemClassifier.load(config.test.classifier_file)
-    total_parameters = enunlg.util.count_parameters(tgen_classifier)
+    total_parameters = enunlg.util.count_parameters(classifier.model)
     if shortcircuit == 'parameters':
         exit()
 
@@ -174,7 +234,7 @@ def test_tgen_classifier(config: omegaconf.DictConfig, shortcircuit=None):
 
     error = 0
     for i, o in test_pairs:
-        prediction = tgen_classifier.predict(i).squeeze(0).squeeze(0).tolist()
+        prediction = classifier.predict(i)
         target_bitvector = np.round(o.tolist())
         output_bitvector = np.round(prediction)
         # print(prediction)
